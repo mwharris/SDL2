67b35209c21e35cc91ecb087e46852b874478f62
mharris161 <mharris161@gmail.com>
2020-02-04 07:35:31 -0500

    Initialize SDL. Create a popup window. Fill the surface of that window with a solid color. Update the displayed surface. Cleanup SDL and the Window memory.

--- SDL2_Tutorials/SDL2_Tutorials/HelloSDL.cpp
+++ SDL2_Tutorials/SDL2_Tutorials/HelloSDL.cpp
@@ -1,55 +1,56 @@
 /*This source code copyrighted by Lazy Foo' Productions (2004-2020)
 and may not be redistributed without written permission.*/
 
-//Using SDL and standard IO
+// Include SDL functions and datatypes
 #include <SDL.h>
+// Just in order to use printf
 #include <stdio.h>
 
-//Screen dimension constants
+// Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 int main(int argc, char* args[])
 {
-	//The window we'll be rendering to
-	SDL_Window* window = NULL;
+	SDL_Window* window;
+	SDL_Surface* screen;
 
-	//The surface contained by the window
-	SDL_Surface* screenSurface = NULL;
-
-	//Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-	}
-	else
-	{
-		//Create window
-		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window == NULL)
-		{
-			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-		}
-		else
-		{
-			//Get window surface
-			screenSurface = SDL_GetWindowSurface(window);
-
-			//Fill the surface white
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-
-			//Update the surface
-			SDL_UpdateWindowSurface(window);
-
-			//Wait two seconds
-			SDL_Delay(2000);
-		}
+	// We must initialize SDL with SDL_Init before we can use SDL functions
+	if (SDL_Init(SDL_INIT_VIDEO)) {
+		printf("Error initializing SDL! SDL_GetError: %s \n", SDL_GetError());
+		return -1;
 	}
 
-	//Destroy window
+	// Create our SDL Window
+	window = SDL_CreateWindow("Hello World!",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		SCREEN_WIDTH,
+		SCREEN_HEIGHT,
+		SDL_WINDOW_SHOWN);
+
+	// Check our window was created properly
+	if (window == NULL) {
+		printf("Error creating the SDL Window! SDL_GetError: %s \n", SDL_GetError());
+	}
+	else {
+		// Get a reference to the surface our window
+		screen = SDL_GetWindowSurface(window);
+
+		// Fill our surface with a color
+		SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 0, 255, 255));
+
+		// Tell SDL to draw our updated surface to the screen
+		SDL_UpdateWindowSurface(window);
+
+		// Wait
+		SDL_Delay(2000);
+	}
+
+	// Clean up our window
 	SDL_DestroyWindow(window);
 
-	//Quit SDL subsystems
+	// Clean up SDL
 	SDL_Quit();
 
 	return 0;
