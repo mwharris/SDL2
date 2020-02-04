4f68df7cd27f398a35b81d17c4947bcac8c6f0a9
mharris161 <mharris161@gmail.com>
2020-02-04 08:40:39 -0500

    You can become a hero. Blit a loaded image to the screen. A bunch of refactoring as well.

--- SDL2_Tutorials/SDL2_Tutorials/HelloSDL.cpp
+++ SDL2_Tutorials/SDL2_Tutorials/HelloSDL.cpp
@@ -7,51 +7,90 @@ and may not be redistributed without written permission.*/
 #include <stdio.h>
 
 // Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 720;
+
+// Forward declare functions to be used in main();
+bool init();
+bool loadMedia();
+void close();
+
+// Global variables for the window and window surface
+SDL_Window* g_window = NULL;
+SDL_Surface* g_screenSurface = NULL;
+SDL_Surface* g_imageSurface = NULL;
 
 int main(int argc, char* args[])
 {
-	SDL_Window* window;
-	SDL_Surface* screen;
-
-	// We must initialize SDL with SDL_Init before we can use SDL functions
-	if (SDL_Init(SDL_INIT_VIDEO)) {
-		printf("Error initializing SDL! SDL_GetError: %s \n", SDL_GetError());
+	// Initialize SDL and anything else we need
+	if (!init()) {
 		return -1;
 	}
+	else {
+		if (!loadMedia()) {
+			return -1;
+		}
+		else {
+			// Blit our image to the screen
+			SDL_BlitSurface(g_imageSurface, NULL, g_screenSurface, NULL);
+			// Tell SDL to draw our updated surface to the screen
+			SDL_UpdateWindowSurface(g_window);
+			// Wait
+			SDL_Delay(3000);
+		}
+	}
 
-	// Create our SDL Window
-	window = SDL_CreateWindow("Hello World!",
-		SDL_WINDOWPOS_UNDEFINED,
-		SDL_WINDOWPOS_UNDEFINED,
-		SCREEN_WIDTH,
-		SCREEN_HEIGHT,
-		SDL_WINDOW_SHOWN);
+	// Free up our window, surface, anything else
+	close();
 
-	// Check our window was created properly
-	if (window == NULL) {
-		printf("Error creating the SDL Window! SDL_GetError: %s \n", SDL_GetError());
+	return 0;
+}
+
+bool init() {
+	// We must initialize SDL with SDL_Init before we can use SDL functions
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		printf("Error initializing SDL! SDL_GetError: %s \n", SDL_GetError());
 	}
 	else {
-		// Get a reference to the surface our window
-		screen = SDL_GetWindowSurface(window);
+		// Create our SDL Window
+		g_window = SDL_CreateWindow("Hello World!",
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
+			SCREEN_WIDTH,
+			SCREEN_HEIGHT,
+			SDL_WINDOW_SHOWN);
 
-		// Fill our surface with a color
-		SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 0, 255, 255));
-
-		// Tell SDL to draw our updated surface to the screen
-		SDL_UpdateWindowSurface(window);
-
-		// Wait
-		SDL_Delay(2000);
+		// Check our window was created properly
+		if (g_window == NULL) {
+			printf("Error creating the SDL Window! SDL_GetError: %s \n", SDL_GetError());
+		}
+		else {
+			// Get a reference to the surface our window
+			g_screenSurface = SDL_GetWindowSurface(g_window);
+			return true;
+		}
 	}
+	return false;
+}
+
+bool loadMedia() {
+	g_imageSurface = SDL_LoadBMP("Images/bnh.bmp");
+	if (g_imageSurface == NULL) {
+		printf("Error while loading BMP file! SDL_GetError: %s \n", SDL_GetError());
+		return false;
+	}
+	return true;
+}
+
+void close() {
+	// Clean up our image surface
+	SDL_FreeSurface(g_imageSurface);
+	g_imageSurface = NULL;
 
 	// Clean up our window
-	SDL_DestroyWindow(window);
+	SDL_DestroyWindow(g_window);
+	g_window = NULL;
 
 	// Clean up SDL
 	SDL_Quit();
-
-	return 0;
 }
